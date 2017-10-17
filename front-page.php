efae4dc1ffcbf9efd5cbba26dbfc13fe8efe6e6d
Thuan Bui <thuanbui@Thuans-MacBook-Pro.local>
2017-10-17 11:22:29 +0700

    modify Front-page.php

--- front-page.php
+++ front-page.php
@@ -12,38 +12,5 @@
 
 get_template_part( 'template', 'full' );
 
-// Remove default page header
-remove_action( 'genesis_after_header', 'business_page_header_open', 20 );
-remove_action( 'genesis_after_header', 'business_page_header_title', 24 );
-remove_action( 'genesis_after_header', 'business_page_header_close', 28 );
-
-
-add_filter( 'genesis_attr_site-inner', 'sk_attributes_site_inner' );
-/**
- * Add attributes for site-inner element.
- *
- * @since 2.0.0
- *
- * @param array $attributes Existing attributes.
- *
- * @return array Amended attributes.
- */
-function sk_attributes_site_inner( $attributes ) {
-	$attributes['role']     = 'main';
-	$attributes['itemprop'] = 'mainContentOfPage';
-	return $attributes;
-}
-// Remove div.site-inner's div.wrap
-add_filter( 'genesis_structural_wrap-site-inner', '__return_empty_string' );
-// Display header
-get_header();
-// Display Content
-the_post(); // sets the 'in the loop' property to true.
-the_content();
-// Display Footer
-get_footer();
-
-
-
 // Run Genesis.
 //genesis();
