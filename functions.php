08086522efd7f7d79c16dd738094c416f31ef81d
Thuan Bui <thuanbui@Thuans-MacBook-Pro.local>
2017-10-25 09:00:54 +0700

    Change product loop column to 3

--- functions.php
+++ functions.php
@@ -406,3 +406,11 @@ function themeprefix_cpt_layout() {
  }
 }
 add_filter( 'genesis_site_layout', 'themeprefix_cpt_layout' );
+
+// Change number or products per row to 3
+add_filter('loop_shop_columns', 'loop_columns');
+if (!function_exists('loop_columns')) {
+	function loop_columns() {
+		return 3; // 3 products per row
+	}
+}
