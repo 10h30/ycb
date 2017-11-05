ba2091f6a17c2879f71aad063b102ad12741d474
Thuan Bui <thuanbui@Thuans-MacBook-Pro.local>
2017-11-05 13:23:26 +0700

    functions.php update

--- functions.php
+++ functions.php
@@ -408,9 +408,10 @@ function themeprefix_cpt_layout() {
 add_filter( 'genesis_site_layout', 'themeprefix_cpt_layout' );
 
 // Change number or products per row to 3
-add_filter('loop_shop_columns', 'loop_columns');
-if (!function_exists('loop_columns')) {
-	function loop_columns() {
-		return 3; // 3 products per row
+add_filter( 'loop_shop_columns', 'tm_product_columns', 5);
+function tm_product_columns($columns) {
+	if ( is_shop() || is_product_category() || is_product_tag() ) {
+		$columns = 3;
+		return $columns;
 	}
 }
